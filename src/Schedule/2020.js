ebb2819aa1e8fc0fc5f3620809b31ecc513cc55f
petermatts <mpeters0514@gmail.com>
2020-07-04 00:49:54 -0400

    Added more to the schedule, about half done.

--- src/Schedule/2020.js
+++ src/Schedule/2020.js
@@ -21,9 +21,9 @@ const game = (away, home, d) => {
     }
 
     let primetime = false;
-    if(day === 'Thursday' || day === 'Monday' || 
-       day === 'Friday' || day === 'Saturday' 
-       || (day === 'Sunday' && time === '8:20 PM')) { 
+    if(day === 'Thursday' || day === 'Monday' ||
+       day === 'Friday' || day === 'Saturday'
+       || (day === 'Sunday' && time === '8:20 PM')) {
         primetime = true;
     }
 
@@ -116,27 +116,117 @@ const schedule = {
         byes: []
     },
     week5: {
-        games: [],
+        games: [
+          game(T[29], T[5], new Date(2020, 9, 8, 20, 20)),
+          game(T[4], T[1], new Date(2020, 9, 11, 13, 0)),
+          game(T[16], T[30], new Date(2020, 9, 11, 13, 0)),
+          game(T[9], T[15], new Date(2020, 9, 11, 13, 0)),
+          game(T[0], T[21], new Date(2020, 9, 11, 13, 0)),
+          game(T[25], T[23], new Date(2020, 9, 11, 13, 0)),
+          game(T[18], T[26], new Date(2020, 9, 11, 13, 0)),
+          game(T[6], T[31], new Date(2020, 9, 11, 13, 0)),
+          game(T[14], T[2], new Date(2020, 9, 11, 13, 0)),
+          game(T[19], T[12], new Date(2020, 9, 11, 16, 5)),
+          game(T[13], T[27], new Date(2020, 9, 11, 16, 25)),
+          game(T[22], T[7], new Date(2020, 9, 11, 16, 25)),
+          game(T[20], T[8], new Date(2020, 9, 11, 20, 20)),
+          game(T[17], T[28], new Date(2020, 9, 12, 20, 15))
+        ],
         byes: [T[10], T[11]]
     },
     week6: {
-        games: [],
+        games: [
+          game(T[15], T[3], new Date(2020, 9, 15, 20, 20)),
+          game(T[12], T[30], new Date(2020, 9, 18, 13, 0)),
+          game(T[6], T[13], new Date(2020, 9, 18, 13, 0)),
+          game(T[1], T[20], new Date(2020, 9, 18, 13, 0)),
+          game(T[31], T[22], new Date(2020, 9, 18, 13, 0)),
+          game(T[2], T[25], new Date(2020, 9, 18, 13, 0)),
+          game(T[7], T[26], new Date(2020, 9, 18, 13, 0)),
+          game(T[5], T[4], new Date(2020, 9, 18, 13, 0)),
+          game(T[10], T[14], new Date(2020, 9, 18, 13, 0)),
+          game(T[19], T[9], new Date(2020, 9, 18, 16, 5)),
+          game(T[23], T[17], new Date(2020, 9, 18, 16, 5)),
+          game(T[11], T[29], new Date(2020, 9, 18, 16, 25)),
+          game(T[18], T[27], new Date(2020, 9, 18, 20, 20)),
+          game(T[0], T[8], new Date(2020, 9, 19, 20, 15)),
+        ],
         byes: [T[16], T[21], T[24], T[28]]
     },
     week7: {
-        games: [],
+        games: [
+          game(T[22], T[25], new Date(2020, 9, 22, 20, 20)),
+          game(T[10], T[1], new Date(2020, 9, 25, 13, 0)),
+          game(T[7], T[6], new Date(2020, 9, 25, 13, 0)),
+          game(T[17], T[19], new Date(2020, 9, 25, 13, 0)),
+          game(T[4], T[24], new Date(2020, 9, 25, 13, 0)),
+          game(T[3], T[23], new Date(2020, 9, 25, 13, 0)),
+          game(T[8], T[31], new Date(2020, 9, 25, 13, 0)),
+          game(T[26], T[2], new Date(2020, 9, 25, 13, 0)),
+          game(T[11], T[12], new Date(2020, 9, 25, 13, 0)),
+          game(T[28], T[0], new Date(2020, 9, 25, 16, 5)),
+          game(T[15], T[9], new Date(2020, 9, 25, 16, 25)),
+          game(T[27], T[21], new Date(2020, 9, 25, 16, 25)),
+          game(T[29], T[16], new Date(2020, 9, 25, 20, 20)),
+          game(T[5], T[18], new Date(2020, 9, 26, 20, 15))
+        ],
         byes: [T[30], T[31], T[20], T[14]]
     },
     week8: {
-        games: [],
+        games: [
+          game(T[1], T[4], new Date(2020, 9, 29, 20, 20)),
+          game(T[21], T[3], new Date(2020, 10, 1, 13, 0)),
+          game(T[30], T[6], new Date(2020, 10, 1, 13, 0)),
+          game(T[16], T[7], new Date(2020, 10, 1, 13, 0)),
+          game(T[13], T[10], new Date(2020, 10, 1, 13, 0)),
+          game(T[20], T[11], new Date(2020, 10, 1, 13, 0)),
+          game(T[23], T[15], new Date(2020, 10, 1, 13, 0)),
+          game(T[18], T[19], new Date(2020, 10, 1, 13, 0)),
+          game(T[14], T[17], new Date(2020, 10, 1, 16, 5)),
+          game(T[24], T[5], new Date(2020, 10, 1, 16, 25)),
+          game(T[27], T[28], new Date(2020, 10, 1, 16, 25)),
+          game(T[8], T[25], new Date(2020, 10, 1, 20, 20)),
+          game(T[29], T[22], new Date(2020, 10, 2, 20, 15))
+
+        ],
         byes: [T[9], T[0], T[26], T[31], T[2], T[12]]
     },
     week9: {
-        games: [],
+        games: [
+          game(T[11], T[27], new Date(2020, 10, 5, 20, 20)),
+          game(T[9], T[1], new Date(2020, 10, 8, 13, 0)),
+          game(T[28], T[3], new Date(2020, 10, 8, 13, 0)),
+          game(T[5], T[30], new Date(2020, 10, 8, 13, 0)),
+          game(T[2], T[13], new Date(2020, 10, 8, 13, 0)),
+          game(T[4], T[15], new Date(2020, 10, 8, 13, 0)),
+          game(T[10], T[20], new Date(2020, 10, 8, 13, 0)),
+          game(T[22], T[31], new Date(2020, 10, 8, 13, 0)),
+          game(T[12], T[14], new Date(2020, 10, 8, 13, 0)),
+          game(T[16], T[17], new Date(2020, 10, 8, 16, 5)),
+          game(T[26], T[8], new Date(2020, 10, 8, 16, 25)),
+          game(T[19], T[0], new Date(2020, 10, 8, 16, 25)),
+          game(T[24], T[29], new Date(2020, 10, 8, 20, 20)),
+          game(T[21], T[23], new Date(2020, 10, 9, 20, 15))
+        ],
         byes: [T[6], T[7], T[18], T[25]]
     },
     week10: {
-        games: [],
+        games: [
+          game(T[13], T[30], new Date(2020, 10, 12, 20, 20)),
+          game(T[12], T[7], new Date(2020, 10, 15, 13, 0)),
+          game(T[31], T[10], new Date(2020, 10, 15, 13, 0)),
+          game(T[14], T[11], new Date(2020, 10, 15, 13, 0)),
+          game(T[25], T[22], new Date(2020, 10, 15, 13, 0)),
+          game(T[6], T[26], new Date(2020, 10, 15, 13, 0)),
+          game(T[29], T[4], new Date(2020, 10, 15, 13, 0)),
+          game(T[9], T[16], new Date(2020, 10, 15, 16, 5)),
+          game(T[23], T[19], new Date(2020, 10, 15, 16, 5)),
+          game(T[3], T[0], new Date(2020, 10, 15, 16, 5)),
+          game(T[27], T[18], new Date(2020, 10, 15, 16, 25)),
+          game(T[28], T[24], new Date(2020, 10, 15, 16, 25)),
+          game(T[2], T[21], new Date(2020, 10, 15, 20, 20)),
+          game(T[20], T[5], new Date(2020, 10, 16, 20, 15))
+        ],
         byes: [T[1], T[8], T[15], T[17]]
     },
     week11: {
